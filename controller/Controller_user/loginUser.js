ef6c86d20e3a050a715e11156aab1741a836a687
FELIPE VIEIRA DOS SANTOS <24122377@dsn127m1249387.cfp127aluno.local>
2025-09-18 14:56:43 -0300

    feat: controller de login concluído

---
+++ controller/Controller_user/loginUser.js
@@ -0,0 +1,46 @@
+/*********************************************************
+ * Autor: Felipe Vieira
+ * Date: 16/09/25
+ * Versão: 1.0
+ * Desc: App que irá realizar as 
+ *       validações dos dados recebidos para LOGIN do Usuário 
+ ********************************************************/
+
+const message = require('../../config/status/status')
+import { loginSQLUser } from "../../model/UserDAO/LoginUser"
+
+const loginUser = async function (user, contentType) {
+    try {
+        
+
+        let dadosLogin = {}
+        if (String(contentType).toLocaleLowerCase == 'application/json') {
+            if (
+                user.email == "" || user.email == undefined || user.email == null || user.email > 100  ||
+                user.senha == "" || user.senha == undefined || user.senha == null || user.senha > 50
+            ){
+                return message.ERROR_REQUIRED_FIELDS
+            }else{
+
+                let resultUser = await loginSQLUser(user)
+                if(resultUser != false || typeof(resultUser) == 'object'){
+                    if (resultUser.length > 0) {
+                        dadosLogin.message = message.SUCCES_LOGIN_COMPLETED
+                        dadosLogin.user = resultUser
+
+                        return dadosLogin
+                    }
+                    return message.ERROR_NOT_FOUND
+                }else{
+                    return message.ERROR_INTERNAL_SERVER_MODEL
+                }
+            }
+        }else{
+            return message.ERROR_CONTENT_TYPE
+        }
+
+    } catch (error) {
+        console.log(error)
+        return message.ERROR_INTERNAL_SERVER_CONTROLLER
+    }
+}

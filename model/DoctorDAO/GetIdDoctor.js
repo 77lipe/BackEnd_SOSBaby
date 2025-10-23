a4bf547c21e87652253cec5d20adcc9408f0da24
77lipe <lipevds.13@gmail.com>
2025-10-23 09:19:33 -0300

    bugfix: médico

--- model/DoctorDAO/GetIdDoctor.js
+++ model/DoctorDAO/GetIdDoctor.js
@@ -12,7 +12,7 @@ const prisma = new PrismaClient()
 export const getIdSQLDoctor = async function(id){
     try {
         
-        let sql = `SELECT * FROM vw_medico_by_id WHERE id = ${id}`
+        let sql = `SELECT * FROM tbl_medico WHERE id_medico = ${id}`
         let resultDoctor = await prisma.$queryRawUnsafe(sql)
 
         if(resultDoctor){
